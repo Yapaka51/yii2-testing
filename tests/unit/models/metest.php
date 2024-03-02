24e631bf6e178e77bc4da84180a1753209bcf819
Yapaka51 <yapaka.sa@mail.wu.ac.th>
2024-03-02 15:30:31 +0700

    criclettest

--- tests/unit/models/metest.php
+++ tests/unit/models/metest.php
@@ -5,8 +5,14 @@ class UserTest extends \Codeception\Test\Unit
 public function testFindUserById(){
     $this->assertTrue(true);
     $this->assertFalse(false);
+    
 
 }
+    public function testFindUserByName()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+    }
 
 }
 
